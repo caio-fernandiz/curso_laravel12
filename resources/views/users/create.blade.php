7f8fd4b8c1410a8905b60737a0bda62d09c865b5
caio <caio@guiafacil.com>
2025-07-03 15:20:17 -0300

    criando header e costumizando ele

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -1,40 +1,58 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" >
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Sistema Ensino </title>
+
+    @vite(['resources/css/app.css'])
+
 </head>
+
 <body>
-    <h1>Cadastrar Usuário</h1>
 
-    @if (session('success'))
-        <p style="color:green"> {{ session('success') }} </p>
+    <div class="main-container">
 
-    @elseif (session('error'))
-        <p style="color:red"> {{ session('error') }} </p>
+        <header class="header">
+            <div class="content-header">
+                <h2 class="title-logo"> <a href="{{ route('dashboard') }}">SisEn</a> </h2>
+                <ul class="list-nav-link">
+                    <li> <a href="#" class="nav-link">Usuário</a> </li>
+                </ul>
+            </div>
+        </header>
 
-    @endif
+        <h1>Cadastrar Usuário</h1>
 
-    <form action="{{ route('user.store') }}" method="POST">
- 
-        @csrf
+        @if (session('success'))
+            <p style="color:green"> {{ session('success') }} </p>
+        @elseif (session('error'))
+            <p style="color:red"> {{ session('error') }} </p>
+        @endif
 
-        <label for="name">Nome:</label>
-        <input type="text" name="name" id="name" placeholder="Nome completo"
-        value="{{ old('name') }}" required><br><br>
+        <form action="{{ route('user.store') }}" method="POST">
 
-        <label for="email">E-mail:</label>
-        <input type="text" name="email" id="email" placeholder="E-mail"
-        value="{{ old('email') }}" required><br><br>
+            @csrf
 
-        <label for="password">Senha:</label>
-        <input type="password" name="password" id="password" placeholder="Senha com no minimo 6 caracteres"
-        value="{{ old('password') }}" required><br><br>
+            <label for="name">Nome:</label>
+            <input type="text" name="name" id="name" placeholder="Nome completo" value="{{ old('name') }}"
+                required><br><br>
 
-        <button type="submit">Cadastrar</button>
+            <label for="email">E-mail:</label>
+            <input type="text" name="email" id="email" placeholder="E-mail" value="{{ old('email') }}"
+                required><br><br>
 
-    </form>
+            <label for="password">Senha:</label>
+            <input type="password" name="password" id="password" placeholder="Senha com no minimo 6 caracteres"
+                value="{{ old('password') }}" required><br><br>
+
+            <button type="submit">Cadastrar</button>
+
+        </form>
+
+    </div>
 </body>
+
 </html>
--- routes/web.php
+++ routes/web.php
@@ -4,8 +4,8 @@ use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('index');
+})->name('dashboard');
 
 Route::get('/create-user', [UserController::class, 'create'])->name('user.create'); 
 
